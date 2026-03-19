390b8b46ce5ad265094129c54b154d53d9a6af1a
TrevorCD <trevorcldrwd@gmail.com>
2026-03-18 21:54:43 -0700

    updated syntax so that terminal_columns and terminal_lines work as globals, and if __name__ bit so that main runs

--- app/app.py
+++ app/app.py
@@ -1,6 +1,22 @@
-# Trevor B. Calderwood
 # Host side application for magic wand communications
 # written to Python 3.14.3 specifications
+#
+#-------------------------------------------------------------------------------
+#
+#   Copyright 2026 Trevor B. Calderwood
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#-------------------------------------------------------------------------------
 
 import bleak    # bluetooth lib
 import shutil   # shell utilities
@@ -14,10 +30,10 @@ terminal_lines   = -1 # terminal height in characters
 # Handlers ---------------------------------------------------------------------
 
 def handle_resize(signum, frame):
+    global terminal_columns, terminal_lines
     assert signum == signal.SIGWINCH, f"Unexpected signal: {signum}"
-    terminal_size = shutil.get_terminal_size()
-    terminal_columns = terminal_size.columns
-    terminal_lines = terminal_size.lines
+    terminal_columns = shutil.get_terminal_size().columns
+    terminal_lines = shutil.get_terminal_size().lines
     
 # end handle_resize
 
@@ -29,16 +45,26 @@ def handle_resize(signum, frame):
 
 def main(argv):
 
+    global terminal_columns, terminal_lines
+    
     signal.signal(signal.SIGWINCH, handle_resize)
     
     # maybe take CLA for size of device area, as in x centimeters of device
     # movement translates to y characters on the terminal
     
-    terminal_size = shutil.get_terminal_size().columns # .columns .lines
+    terminal_columns = shutil.get_terminal_size().columns
+    terminal_lines   = shutil.get_terminal_size().lines
 
+    print(terminal_columns)
+    
     while(1):
-
+        assert (terminal_columns != -1 and terminal_lines != -1)
+        
         
     #end while
     
 # end main
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
