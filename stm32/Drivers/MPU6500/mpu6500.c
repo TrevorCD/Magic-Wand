c1674627b91466d4536b42f56888218ef58f2381
TrevorCD <trevorcldrwd@gmail.com>
2026-03-26 16:25:41 -0700

    added files for mpu6500 device driver. Just description, includes, and license for now.

---
+++ stm32/Drivers/MPU6500/mpu6500.c
@@ -0,0 +1,27 @@
+/* mpu6500.c
+ *
+ * Device driver source for MPU-6500 Accelerometer. Depends on stm32's HAL.
+ *
+ *-----------------------------------------------------------------------------
+ *
+ *   Copyright 2026 Trevor B. Calderwood
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *----------------------------------------------------------------------------*/
+
+/* Hardware Includes */
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+
+#include "mpu6500.h"
